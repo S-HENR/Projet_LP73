232d61576e1d9385cd815280eb00b50f1514776a
S-HENR <seraphin.henry@orange.fr>
2020-06-26 23:21:59 +0200

    nb Queen added to information display

--- Fourmis/anthillstatus.cpp
+++ Fourmis/anthillstatus.cpp
@@ -13,12 +13,13 @@ AnthillStatus::~AnthillStatus()
     delete ui;
 }
 
-void AnthillStatus::display(int nb_ant, int nb_egg, int nb_larva, int nb_worker, int nb_warrior,int nb_max_ant, int amount_food, int max_amount_food)
+void AnthillStatus::display(int nb_ant, int nb_egg, int nb_larva, int nb_worker, int nb_warrior, int nb_queen, int nb_max_ant, int amount_food, int max_amount_food)
 {
     ui->label_nb_ant->setText(QString::number(nb_ant) + "/" + QString::number(nb_max_ant));
     ui->label_nb_egg->setText(QString::number(nb_egg));
     ui->label_nb_larva->setText(QString::number(nb_larva));
     ui->label_nb_worker->setText(QString::number(nb_worker));
     ui->label_nb_warrior->setText(QString::number(nb_warrior));
+    ui->label_nb_queen->setText(QString::number(nb_queen));
     ui->label_amount_food->setText(QString::number(amount_food) + "/" + QString::number(max_amount_food));
 }
--- Fourmis/anthillstatus.h
+++ Fourmis/anthillstatus.h
@@ -14,7 +14,7 @@ class AnthillStatus : public QMainWindow
 public:
     explicit AnthillStatus(QWidget *parent = nullptr);
     ~AnthillStatus();
-    void display(int nb_ant, int nb_egg, int nb_larva, int nb_worker, int nb_warrior,int nb_max_ant, int amount_food, int max_amount_food);
+    void display(int nb_ant, int nb_egg, int nb_larva, int nb_worker, int nb_warrior, int nb_queen, int nb_max_ant, int amount_food, int max_amount_food);
 
 private:
     Ui::AnthillStatus *ui;
